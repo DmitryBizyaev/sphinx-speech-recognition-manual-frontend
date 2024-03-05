9e6317c1c4538b57ec3fdb7e42b400e62c8afaf9
Dmitry Bizyaev <83307719+DmitryBizyaev@users.noreply.github.com>
2024-03-06 01:16:25 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo $"\napt-get update"
+echo "apt-get update"
 sudo apt-get update &> /dev/null
 
-echo $"\ncheck python version"
+echo "check python version"
 sudo python3 --version 2> /dev/null
 
-echo $"\npip install"
+echo "pip install"
 sudo apt-get install python3-pip -y &> /dev/null
 sudo pip3 --version 2> /dev/null
 
-echo $"\npip update"
+echo "pip update"
 sudo pip3 install --upgrade pip -y &> /dev/null
 sudo pip3 --version 2> /dev/null
 
-echo $"\nflask install"
+echo "flask install"
 sudo pip3 install Flask &> /dev/null
 sudo pip3 show Flask 2> /dev/null
 
-echo $"\nserver start"
-sudo flask run --port=5555
+echo "server start"
+sudo flask run --host=0.0.0.0 --port=5555
