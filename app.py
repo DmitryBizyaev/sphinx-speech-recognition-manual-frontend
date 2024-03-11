ec26982533c49de003b76b119d99a1219a403caf
Dmitry Bizyaev <83307719+DmitryBizyaev@users.noreply.github.com>
2024-03-11 22:41:32 +0300

    app and index update

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def front_server():
+def starting_state():
 
     url = "http://localhost:5000"
     output_text = "upload a file first!"
+    return render_template('index.html', output_text=output_text, url=url)
+
+
+@app.route("/test", methods=['POST'])
+def main_state():
+
+    url = "http://localhost:5000"
+    output_text = "upload a file first!"
+    # r = requests.post(url)
+    print("received POST request")
     return render_template('index.html', output_text=output_text, url=url)
     
